2950d799fd45d9a60f96e1ca99c0ce86641c8bc8
Amy Qiu <amy-qiu@outlook.com>
2016-08-20 22:52:01 -0400

    Made investment class

---
+++ app/src/main/java/com/daph/accountable/NutritionChallenge.java
@@ -0,0 +1,40 @@
+package com.daph.accountable;
+
+/**
+ * Created by Amy on 2016-08-20.
+ */
+public class NutritionChallenge extends Challenge {
+
+    private Nutrition nutrition;
+
+    public NutritionChallenge(int newAmount, User newRequester, User newRecipient, Nutrition newNutrition)
+    {
+        amount = newAmount;
+        requester = newRequester;
+        recipient = newRecipient;
+        nutrition = newNutrition;
+    }
+
+    public void setNutrition(Nutrition newNutrition)
+    {
+        nutrition = newNutrition;
+    }
+
+    public Nutrition getNutrition()
+    {
+        return nutrition;
+    }
+
+    public void completeNutrition()
+    {
+        recipient.addNutrition(nutrition);
+        recipient.addPoints(amount * 2);
+    }
+
+    public void failNutrition()
+    {
+        recipient.addPoints(amount * -1);
+        requester.addPoints(amount * 2);
+    }
+
+}
