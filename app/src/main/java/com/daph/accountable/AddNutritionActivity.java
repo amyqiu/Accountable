39e6513775b991470c1ab83faeceb8e5cb9194b6
Denizkusu <derin.denizkusu@gmail.com>
2016-08-21 09:14:04 -0400

    Nutrition activity commit

--- app/src/main/java/com/daph/accountable/AddNutritionActivity.java
+++ app/src/main/java/com/daph/accountable/AddNutritionActivity.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import com.daph.accountable.model.Constants;
+import com.daph.accountable.model.Nutrition;
 
 public class AddNutritionActivity extends AppCompatActivity {
 
@@ -14,6 +18,19 @@ public class AddNutritionActivity extends AppCompatActivity {
     }
 
     public void nutritionNext(View view) {
+        String accName = Constants.globalAccomplishment.getName();
+        String accDesc = Constants.globalAccomplishment.getDescription();
+
+
+        TextView protein = (TextView) findViewById(R.id.proteinIntake);
+        TextView calories = (TextView) findViewById(R.id.Calorie);
+
+        Nutrition newNutrition = new Nutrition(2500, 140,Integer.parseInt(calories.toString()),
+                Integer.parseInt(protein.toString())); // Calories = 2500, protein = 140 hardcoded
+
+        Constants.globalUser.addNutrition(newNutrition);
+
+
         Intent intent = new Intent(AddNutritionActivity.this, Splash.class);
         // Change the above to (AddNutritionActivity.this, splash.class);
         // the "splash" page says "Your Plant Gru!" with a back to menu button and maybe some
