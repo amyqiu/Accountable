704193b1697cf4ae33dae193c6c5f1104f8c5507
Denizkusu <derin.denizkusu@gmail.com>
2016-08-21 07:09:07 -0400

    Layout stuff and some sting values. Plus gradle but I have no idea what changed tbh sorry

--- app/src/main/java/com/daph/accountable/AddMeditationActivity.java
+++ app/src/main/java/com/daph/accountable/AddMeditationActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.daph.accountable.R;
+import com.daph.accountable.model.Constants;
 
 public class AddMeditationActivity extends AppCompatActivity {
 
@@ -16,6 +17,10 @@ public class AddMeditationActivity extends AppCompatActivity {
     }
 
     public void meditationNext(View view) {
+        String accName = Constants.globalAccomplishment.getName();
+        String accDesc = Constants.globalAccomplishment.getDescription();
+
+
         Intent intent = new Intent(AddMeditationActivity.this, AddWorkoutActivity.class);
         // Change the above to (AddMeditationActivity.this, splash.class);
         // the "splash" page says "Your Plant Gru!" with a back to menu button and maybe some
