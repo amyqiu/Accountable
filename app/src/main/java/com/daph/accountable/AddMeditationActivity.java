e04e32c497da693d88b5b65ada6f0995093ef228
pbhattachan <presish.bhattachan@gmail.com>
2016-08-21 06:07:17 -0400

    Added activity for meditation

---
+++ app/src/main/java/com/daph/accountable/AddMeditationActivity.java
@@ -0,0 +1,25 @@
+package com.daph.accountable;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.daph.accountable.R;
+
+public class AddMeditationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_meditation);
+    }
+
+    public void meditationNext(View view) {
+        Intent intent = new Intent(AddMeditationActivity.this, AddWorkoutActivity.class);
+        // Change the above to (AddMeditationActivity.this, splash.class);
+        // the "splash" page says "Your Plant Gru!" with a back to menu button and maybe some
+        // display shit (like how many gains you got, and your XP until next level)
+
+    }
+}
