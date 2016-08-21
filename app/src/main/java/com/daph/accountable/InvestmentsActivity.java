c50e3a2af8b5a015d177b1f6984e828d9fa0dd1e
Amy Qiu <amy-qiu@outlook.com>
2016-08-21 11:17:56 -0400

    Added investments stuff

--- app/src/main/java/com/daph/accountable/InvestmentsActivity.java
+++ app/src/main/java/com/daph/accountable/InvestmentsActivity.java
@@ -1,6 +1,8 @@
 package com.daph.accountable;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,6 +12,11 @@ import android.view.View;
 
 import com.daph.accountable.model.Constants;
 import com.daph.accountable.model.Investment;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,8 +36,36 @@ public class InvestmentsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_investments);
 
-        seedMoney = Constants.globalUser.getInvestmentsInMe();
-        investments = Constants.globalUser.getMyInvestments();
+        SharedPreferences prefs = this.getSharedPreferences(
+                "com.daph.accountable", Context.MODE_PRIVATE);
+        final String name = prefs.getString("com.daph.accountable.name", "");
+
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef = firebaseDatabase.getReference("investments");
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                allInvestments = Investment.stringListToInvestmentList((ArrayList<String>)dataSnapshot.getValue());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        for (Investment investment:
+             allInvestments) {
+            if (investment.getRecipient().getName() == name)
+            {
+                seedMoney.add(investment);
+            }
+            else
+            {
+                investments.add(investment);
+            }
+
+        }
 
         seedMoneyView = (RecyclerView) findViewById(R.id.seed_money_view);
         investmentsView = (RecyclerView) findViewById(R.id.investments_view);
