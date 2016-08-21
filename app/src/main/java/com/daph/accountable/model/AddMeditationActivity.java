687d0e83decd77fd2f4f5f7f88cf4683b25b8087
pbhattachan <presish.bhattachan@gmail.com>
2016-08-21 07:12:47 -0400

    Added activity for meditation

---
+++ app/src/main/java/com/daph/accountable/model/AddMeditationActivity.java
@@ -0,0 +1,13 @@
+package com.daph.accountable.model;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddMeditationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_meditation2);
+    }
+}
